23d5dc7d4d822967c72e0eed90e5f47b66926287
patchwork <patchwork003@gmail.com>
2025-01-27 11:52:49 -0800

    completion of basic function_lab

--- rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65.d
+++ rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65.d
@@ -1,5 +1,5 @@
-/home/patchwork/rustProject/rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65: src/main.rs
+/home/patchwork/work_rust_learning/rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65: src/main.rs
 
-/home/patchwork/rustProject/rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65.d: src/main.rs
+/home/patchwork/work_rust_learning/rustLearning/function_lab/target/debug/deps/function_lab-adc80d6ccae6cc65.d: src/main.rs
 
 src/main.rs:
--- rustLearning/function_lab/target/debug/function_lab.d
+++ rustLearning/function_lab/target/debug/function_lab.d
@@ -1,1 +1,1 @@
-/home/patchwork/rustProject/rustLearning/function_lab/target/debug/function_lab: /home/patchwork/rustProject/rustLearning/function_lab/src/main.rs
+/home/patchwork/work_rust_learning/rustLearning/function_lab/target/debug/function_lab: /home/patchwork/work_rust_learning/rustLearning/function_lab/src/main.rs
