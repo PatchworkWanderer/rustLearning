37ad16d9905c08faa99cd1d1a687717d15c4d41b
patchwork <patchwork003@gmail.com>
2025-01-28 11:52:54 -0800

    updated notes and main.rs for ownership_lab

--- rustLearning/ownership_lab/src/main.rs
+++ rustLearning/ownership_lab/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
-}
+	//creating a string that allocates data on the heap making it mutable 
+	let mut s = String::from("hello"); // s is now in scope
+
+	s.push_str(", world!"); //appends a litteral to the string
+
+	println!("{s}"); 
+
+
+
+} //scope is now over and s is no longer valid
