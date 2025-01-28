f03f28889090b886fcb674786d3df98febd0f671
patchwork <patchwork003@gmail.com>
2025-01-27 21:14:15 -0800

    completed control_flow_lab and control_flow_notes

--- rustLearning/control_flow_lab/src/main.rs
+++ rustLearning/control_flow_lab/src/main.rs
@@ -18,9 +18,16 @@ fn main() {
 	let conditional_number = if example_condition {6} else {9};
 	println!("the conditional number is: {conditional_number}");
 
-	//loop section
-	loop_types(30);
+	// loop section
+	// uncomment each function below to test
+		//passing_value_loop();
+		//loop_types(30);
+		//loop_labels();
+		//while_loop();
+		for_loop();
+}
 
+fn passing_value_loop() {
 	//passing the value of a loop to the rest of the code
 	let mut counter = 0;
 	let result = loop {
@@ -31,10 +38,7 @@ fn main() {
 		}
 	};
 	println!("The result of the loop is {result}");
-
-
 }
-
 
 fn loop_types(loop_iterations: u16) {
 	// starts a loop that will break after counter == loop_iterations
@@ -53,3 +57,74 @@ fn loop_types(loop_iterations: u16) {
 		}
 	}
 }
+
+fn loop_labels() {
+	// function to demonstrate usage of loop labels
+	let mut i = 0;
+	//'counting_up is the loop label
+	'counting_up: loop{
+		println!("count = {i}");
+		let mut remaining = 10;
+		
+		loop {
+			println!("remaining = {remaining}");
+			if remaining == 9 {	
+				break; // this break will only exit the inner loop
+			 }
+			if i == 2 {
+				break 'counting_up; //using the loop label to break here will exit the outer most loop leaving i == 2
+			}
+			remaining -=1;
+		}
+		i +=1;
+	}
+	println!("End count = {i}");
+}
+
+
+fn while_loop() {
+	// demonstrates the use of while loops
+	let mut i = 10;
+	
+	while i != 0 {
+		println!("{i}");
+		i-=1;
+	}
+	println!("Lift off!!");
+}
+
+fn for_loop() {
+	// demonstrates the use of for loops primairly used for iterating through a list
+	let simple_array = [10,20,30,40,50,60,70,80,90,100];
+
+	for element in simple_array {
+		println!("the value is: {element}");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
