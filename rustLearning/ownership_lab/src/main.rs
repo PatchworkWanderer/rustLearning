1ea01c537696c85022bf851a4f7f3d57f94a55e5
patchwork <patchwork003@gmail.com>
2025-01-28 13:20:18 -0800

    updated

--- rustLearning/ownership_lab/src/main.rs
+++ rustLearning/ownership_lab/src/main.rs
@@ -5,7 +5,34 @@ fn main() {
 	s.push_str(", world!"); //appends a litteral to the string
 
 	println!("{s}"); 
+	
+	//moving_variables();
+	deep_copy();
+
+} 
+// at the ending curly bracket rust calls the drop function 
+// where the author of string can put the code to return the memory
+
+// scope is now over and s is no longer valid and the memory
+// assigned to it is released.
+
+fn moving_variables() {
+
+	// x & y are both the same and are set to the same size
+	// cause they are a know fixed size and are pushed to the stack
+	let _x = 5;
+	let _y = _x;
+
+	// str2 doesn't copy just the value from str1 but the 
+	// ptr, len, and capacity of str1 because it is allocated to the heap
+	let str1 = String::from("hello");
+	let str2 = str1; //after declaring str2 rust will consider str1 invalid
+	
+	println!("{str2}"); 
+		//attempting to run str1 here will cause a compilation error
+}
+
+fn deep_copy() {
 
 
-
-} //scope is now over and s is no longer valid
+}
