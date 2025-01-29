f9520d39ef86e240a23e963b3f5ab350f5cb9ed9
patchwork <patchwork003@gmail.com>
2025-01-29 15:14:13 -0800

    add new sections to ownership_lab materials

--- rustLearning/ownership_lab/src/main.rs
+++ rustLearning/ownership_lab/src/main.rs
@@ -6,8 +6,18 @@ fn main() {
 
 	println!("{s}"); 
 	
+	// below are functions that demonstrate various concepts on ownership
+	// and the difference between data stored on the stack vs. heap
+	// uncomment any of them to run.
+
 	//moving_variables();
-	deep_copy();
+	//deep_copy();
+	//stack_copy();
+	//ownership();
+	return_ownership();
+
+
+
 
 } 
 // at the ending curly bracket rust calls the drop function 
@@ -41,3 +51,109 @@ fn deep_copy() {
 	println!("str1 = {str1}, str2 = {str2}");
 
 }
+
+fn stack_copy() {
+	// with data on the stack since they are know in size it is
+	// possible to copy all the data from the variables directly from
+	// the stack. by using the copy method secretly	
+
+	let int1 = 5;
+	let int2 = int1;
+
+	println!("int1 = {int1}, int2 = {int2}");	
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+fn ownership() {
+	let str1 = String::from("hello"); // str1 comes into scope and is
+									  // allocated on the heap
+	
+	take_ownership(str1);			  // str1's value is moved into the
+									  // function and is no longer valid
+									  // here
+
+	//println!("{str1}");             // this will cause rust to throw a
+									  // compilation because of the move
+
+	let int1 = 5;					  // int1 comes into scope
+
+	make_copy(int1);				  // since int1 is able to implement
+									  // the copy method int1 is still 
+									  // able to be used here
+
+	println!("{int1}");  		      // since int1 was only copied it is
+									  // still able to be used in scope
+
+} // int1 goes out of scope, then str1. However, since str1's value was
+  // moved, nothing special happens 
+
+fn take_ownership(str2: String) { //str2 comes into scope
+	println!("{str2}");
+} //str2 goes out of scope and 'drop' is called freeing the memory
+
+fn make_copy(int2: i32) { // int2 is called into scope
+	println!("{int2}");
+} // int2 goes out of scope, nothing special happens
+
+//////////////////////////////////////////////////////////////////////////
+
+fn return_ownership() { 
+	let str1 = gives_ownership();	// gives_ownership moves its return
+									// value into str1
+
+	let str2 = String::from("hello"); //str2 comes into scope
+
+	let str3 = takes_and_gives_back_ownership(str2); // str2 is moved into
+								    // the function, which also moves its 
+									// value into 
+	println!("{str1}");
+}
+// str3 goes out of scope and is dropped, str2 was moved so nothing
+// happens, and str1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String {
+//fn will move its return value into the function that calls it
+	let str4 = String::from("this is from inside the function"); 
+		// str4 comes into scope
+
+	str4 // str4 is returned and is moved out to the calling fn	
+}
+
+fn takes_and_gives_back_ownership(str5: String) -> String {
+// fn will move the value from the parameter passed into the fn
+// and return a string into another variable
+	str5
+}
+
+///////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
